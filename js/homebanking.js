d010e9b075159a592a89cf17151d7030cf150706
Dony Cisneros <dony.cisne@gmail.com>
2017-12-13 01:05:59 -0300

    Fix de validación

--- js/homebanking.js
+++ js/homebanking.js
@@ -31,25 +31,17 @@ function restarDinero(cantidad) {
 }
 
 function cambiarLimiteDeExtraccion() {
-    var cambiarLimiteDeExtraccion = prompt(
-        "Ingrese un nuevo límite de extracción:"
-    );
+    var cambiarLimiteDeExtraccion = prompt("Ingrese un nuevo límite de extracción:");
 
     if (cambiarLimiteDeExtraccion == null) {
-        alert(
-            "No se ingreso el nuevo límite de Extracción."
-        );
+        alert("No se ingreso el nuevo límite de Extracción.");
     }
     else if (isNaN(cambiarLimiteDeExtraccion)) {
-        alert(
-            "Ingresa solo el monto."
-        );
+        alert("Ingresa solo el monto.");
     }
     else {
         limiteExtraccion = parseInt(cambiarLimiteDeExtraccion);
-        alert(
-            "El nuevo límte de extraccón es: $" + limiteExtraccion
-        );
+        alert("El nuevo límte de extraccón es: $" + limiteExtraccion);
         actualizarLimiteEnPantalla();
     }
 }
@@ -63,43 +55,31 @@ function mostrarOperacion(alertaDeOperacion, accionDeOperacion, transaccionDeDin
 }
 
 function extraerDinero() {
-    var extraerDinero = prompt(
-        "Ingrese la cantidad de dinero que desea extraer:"
-    );
-    
+    var extraerDinero = prompt("Ingrese la cantidad de dinero que desea extraer:");
+
     if (extraerDinero == null) {
-        alert(
-            "No se ingreso monto para extraer."
-        );
+        alert("No se ingreso monto para extraer.");
     }
     else {
         dineroExtraido = parseInt(extraerDinero);
 
         if (dineroExtraido > saldoCuenta ) {
-            alert(
-                "No hay saldo disponible para extraer esa cantidad de dinero."
-            );
-        } 
+            alert("No hay saldo disponible para extraer esa cantidad de dinero.");
+        }
         else if (dineroExtraido > limiteExtraccion ) {
-            alert(
-                "La cantidad que deseas extraer es mayor a tu límite de extracción."
-            );
+            alert("La cantidad que deseas extraer es mayor a tu límite de extracción.");
         }
         else if (isNaN(dineroExtraido)){
-            alert(
-                "Solo puedes ingresar el monto."
-            );
-        }        
+            alert("Solo puedes ingresar el monto.");
+        }
         else if (extraerDinero % 100 !== 0) {
-            alert(
-                "Solo puedes extraer billetes de 100."
-            );
+            alert("Solo puedes extraer billetes de 100.");
         }
         else {
             restarDinero(dineroExtraido);
             mostrarOperacion(
-                "retirado", 
-                saldoCuenta + dineroExtraido, 
+                "retirado",
+                saldoCuenta + dineroExtraido,
                 dineroExtraido
             );
             actualizarSaldoEnPantalla();
@@ -107,29 +87,44 @@ function extraerDinero() {
     }
 }
 
-function depositarDinero() {
-    dineroDepositado = prompt(
-        "Ingrese la cantidad de dinero que desea depositar:"
+function mostrarOperacion(alertaDeOperacion, accionDeOperacion, transaccionDeDinero) {
+    alert(
+        "Has " + alertaDeOperacion + ": $" + transaccionDeDinero + "\n" +
+        "Saldo anterior: $" + accionDeOperacion + "\n" +
+        "Saldo actual: $" + saldoCuenta
     );
+}
+
+function depositarDinero() {
+    dineroDepositado = prompt("Ingrese la cantidad de dinero que desea depositar:");
 
     if (dineroDepositado == null) {
-        alert(
-            "No se ingreso monto para dipositar."
-        );
+        alert("No se ingreso monto para dipositar.");
     }
     else if (isNaN(dineroDepositado)) {
-        alert(
-            "Ingresa solo el monto."
-        );
+        alert("Ingresa solo el monto.");
     }
     else {
         dineroDepositado = parseFloat(dineroDepositado);
         sumarDinero(dineroDepositado);
-        mostrarOperacion(
-            "depositado", 
-            saldoCuenta - dineroDepositado, 
-            dineroExtraido
+        mostrarOperacion("depositado", saldoCuenta - dineroDepositado, dineroDepositado);
+        actualizarSaldoEnPantalla();
+    }
+}
+
+function pagoDeServicio(servicio, precioDeServicio) {
+
+    if (saldoCuenta < precioDeServicio) {
+        alert("No hay suficiente saldo para pagar este servicio.");
+    }
+    else {
+        alert(
+            "Has pagado el servicio " + servicio + ".\n" +
+            "Saldo anterior: $" + saldoCuenta + "\n" +
+            "Dinero descontado: $" + precioDeServicio + "\n" +
+            "Saldo actual: $" + (saldoCuenta - precioDeServicio)
         );
+        restarDinero(precioDeServicio);
         actualizarSaldoEnPantalla();
     }
 }
@@ -140,101 +135,57 @@ function pagarServicio() {
         "1 - Agua" + "\n" +
         "2 - Luz"  + "\n" +
         "3 - Internet" + "\n" +
-        "4 - Teléfono" + "\n" 
+        "4 - Teléfono" + "\n"
     );
-    
+
     if (servicioAPagar == null) {
-        alert(
-            "No ingreso ninguna opción."
-        );
+        alert("No ingreso ninguna opción.");
     }
     else {
         servicioAPagar = parseInt(servicioAPagar);
 
-        function pagoDeServicio(servicio, precioDeServicio) {
-
-            if (saldoCuenta < precioDeServicio) {
-                alert(
-                    "No hay suficiente saldo para pagar este servicio."
-                );
-            } 
-            else {
-                alert(
-                    "Has pagado el servicio " + servicio + ".\n" +
-                    "Saldo anterior: $" + saldoCuenta + "\n" +
-                    "Dinero descontado: $" + precioDeServicio + "\n" +
-                    "Saldo actual: $" + (saldoCuenta - precioDeServicio)
-                );
-                restarDinero(precioDeServicio);
-                actualizarSaldoEnPantalla();
-            }
-        }
-
         switch (servicioAPagar) {
             case 1:
-                pagoDeServicio(
-                    "Agua", precioDeServicioAgua
-                );
+                pagoDeServicio("Agua", precioDeServicioAgua);
                 break;
             case 2:
-                pagoDeServicio(
-                    "Luz", precioDeServicioLuz
-                );
+                pagoDeServicio("Luz", precioDeServicioLuz);
                 break;
             case 3:
-                pagoDeServicio(
-                    "Internet", precioDeServicioInternet
-                );
+                pagoDeServicio("Internet", precioDeServicioInternet);
                 break;
             case 4:
-                pagoDeServicio(
-                    "Teléfono", precioDeServicioTelefono
-                );
-                break;            
+                pagoDeServicio("Teléfono", precioDeServicioTelefono);
+                break;
             default:
-                alert(
-                    "No existe el servicio que se ha seleccionado."
-                );
+                alert("No existe el servicio que se ha seleccionado.");
                 break;
         }
     }
 }
 
 function transferirDinero() {
-    var montoATransferir = prompt(
-        "Ingrese el monto que desea transferir:"
-    );
+    var montoATransferir = prompt("Ingrese el monto que desea transferir:");
 
     if(saldoCuenta < montoATransferir) {
-        alert(
-            "No puede transferirse esa cantidad de dinero."
-        );
+        alert("No puede transferirse esa cantidad de dinero.");
     }
     else if (montoATransferir == null) {
-        alert(
-            "No ingreso monto para la transferencia."
-        );
+        alert("No ingreso monto para la transferencia.");
     }
-    else if (isNaN(montoATransferir)) {        
-        alert(
-            "Ingrese solo el monto."
-        );
+    else if (isNaN(montoATransferir)) {
+        alert("Ingrese solo el monto.");
     }
     else{
-        var cuentaATransferir = prompt(
-            "Ingrese el número de cuenta al que desea transferir el dinero:"
-        );
+        var cuentaATransferir = prompt("Ingrese el número de cuenta al que desea transferir el dinero:");
 
         if (cuentaATransferir == null) {
-            alert(
-                "No ingreso cuenta para la transferencia."
-            );
+            alert("No ingreso cuenta para la transferencia.");
         }
-
         else {
             restarDinero(montoATransferir);
             cuentaATransferir = parseInt(cuentaATransferir);
-    
+
             function tranferirACuenta(cuenta) {
                 alert(
                     "Se han transferido $" + montoATransferir + "\n" +
@@ -242,7 +193,6 @@ function transferirDinero() {
                 );
                 actualizarSaldoEnPantalla();
             }
-    
             switch (cuentaATransferir) {
                 case cuentaAmiga1:
                     tranferirACuenta(cuentaAmiga1);
@@ -251,9 +201,7 @@ function transferirDinero() {
                     tranferirACuenta(cuentaAmiga2);
                     break;
                 default:
-                    alert(
-                        "Solo puede transferirse dinero a una cuenta amiga."
-                    );
+                    alert("Solo puede transferirse dinero a una cuenta amiga.");
                     break;
             }
         }
@@ -261,33 +209,25 @@ function transferirDinero() {
 }
 
 function depositarCheque() {
-    var montoDecheque =  prompt(
-        "Ingrese el monto del cheque:"
-    );
+    var montoDecheque =  prompt("Ingrese el monto del cheque:");
 
     if (montoDecheque == null) {
-        alert(
-            "No ingreso monto para el deposito."
-        );
+        alert("No ingreso monto para el deposito.");
     }
     else {
         var montochequeDepositado = parseInt(montoDecheque);
 
         if (isNaN(montochequeDepositado)){
-            alert(
-                "Ingrese solo el monto."
-            );
-        }        
+            alert("Ingrese solo el monto.");
+        }
         else if (montochequeDepositado < 1000) {
-            alert(
-                "Solo se aceptan cheques desde $1000"
-            );
+            alert("Solo se aceptan cheques desde $1000");
         }
         else {
             sumarDinero(montochequeDepositado);
             mostrarOperacion(
-                "depositado un cheque con el monto", 
-                saldoCuenta - montochequeDepositado, 
+                "depositado un cheque con el monto",
+                saldoCuenta - montochequeDepositado,
                 montochequeDepositado
             );
             actualizarSaldoEnPantalla();
@@ -297,31 +237,24 @@ function depositarCheque() {
 
 window.onload(iniciarSesion());
 
-function iniciarSesion() {
-    var codigoVerificacion = prompt(
-        "Ingresa tu código de verificación:"
-    );
+function alertaCodigoIncorrecto(mensaje) {
+    alert(mensaje);
+    nombreUsuario = "";
+    saldoCuenta = 0;
+    actualizarSaldoEnPantalla();
+    actualizarNombreEnPantalla("");
+}
 
-    function alertaCodigoIncorrecto(mensaje) {
-        alert(mensaje);
-        nombreUsuario = "";
-        saldoCuenta = 0;
-        actualizarSaldoEnPantalla();
-        actualizarNombreEnPantalla("");
-    }
+function iniciarSesion() {
+    var codigoVerificacion = prompt("Ingresa tu código de verificación:");
 
     if (codigoVerificacion == null) {
-        alertaCodigoIncorrecto(
-            "Usted no ingreso un código de verficación."
-        );
-    } 
+        alertaCodigoIncorrecto("Usted no ingreso un código de verficación.");
+    }
     else {
-
         if (codigoVerificacion == codigoCuenta) {
-            alert(
-                "Bienvenido/a " + nombreUsuario + " ya puedes comenzar a realizar operaciones."
-            );
-        } 
+            alert("Bienvenido/a " + nombreUsuario + " ya puedes comenzar a realizar operaciones.");
+        }
         else {
             alertaCodigoIncorrecto(
                 "Código incorrecto." + "\n\n" +
